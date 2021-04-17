1d139650faa49797bfc10476aefbe9699a4ba1e0
Joaquin Sven Svendsen <joaquinsvendsen90@mi.unc.edu.ar>
2021-04-17 17:06:55 -0300

    Calculadora andando con todas las operaciones

--- suma_binarios.asm
+++ suma_binarios.asm
@@ -2,7 +2,6 @@ segment .data
 numero1 dw 0        ;declaro una variable de 16 bits, inicializada en 0
 numero2 dd 0
 
-;cadena  db "OVERFLOW", 0
 segment .bss
 entrada2  resd 1 
 segment .text
@@ -18,25 +17,7 @@ suma_binarios:
     mov eax, [ebp + 8]      ;el primer parametro [ebp + 8] lo copio al registro ax
     add eax, [numero2]      ;realizo la suma y el resultado lo guardo en ax.
     
-    ;jc contiene_cf         ;si el resultado de la suma es mayor a 16 bits, me va a dar carry, y si me da carry, saldo a la etiqueta excedente
-
     leave
     ret
 
-;contiene_cf:
-    ;pusha
-    ;pushf
-
-    ;mov eax, cadena
-    ;call imprimir_cadena
-    ;call salto_linea
-
-    ;popf
-    ;pusha
-
-    ;leave
-
-    ;mov eax, 1                ;con la int 0x80 se le notifica al nucleo que queremos hacer una llamada al sistema
-    ;mov ebx, 0                ;un valor de 1 en eax, quiere decir que queremos hacer una llamada de exit().
-    ;int 0x80                  ;ya que si tenemos overflow en el resultado de la suma, queremos terminar el programa.
 
